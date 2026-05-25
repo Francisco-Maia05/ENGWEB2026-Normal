d2f1a677c55633d614bf0fc92c65ff74c4bfd81d
Francisco Queirós  Ribeiro da Costa Maia <a108962@uminho.pt>
2026-05-25 12:29:30 +0100

    Create livro.js

---
+++ ex2/models/livro.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const livroSchema = new mongoose.Schema({
+    titulo: {
+        type: String,
+        required: true
+    },
+    autor: {
+        type: String,
+        required: true
+    },
+    paginas: {
+        type: Number,
+        required: true
+    },
+    genero: {
+        type: String,
+        required: true
+    },
+    lido: {
+        type: Boolean,
+        default: false
+    }
+}, { 
+    versionKey: false,
+    collection: 'livros' 
+});
+
+module.exports = mongoose.model('livro', livroSchema);
